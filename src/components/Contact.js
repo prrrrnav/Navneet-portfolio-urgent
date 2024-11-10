b2e8398566ae6b506ee02c824f1ea9283261fd45
Pranav <Pranav143010@gmail.com>
2024-11-10 17:17:55 +0530

    updated links

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,7 +7,7 @@ const Contact = () => (
         <p>Connect with me on social media or shoot me an email!</p>
         <div className='flex flex-row space-x-5 text-gray-600 pt-5'>
             <a href="https://m.facebook.com/story.php/?story_fbid=795406898177115&id=2049223398625857" target="_blank" >Fb</a>
-            <a href="https://www.instagram.com/yourinstagramprofile" target="_blank" >Instagram</a>
+            <a href="https://www.instagram.com/king.navneet.singh/" target="_blank" >Instagram</a>
             <a href="https://kingnavneetsingh.blogspot.com/" target="_blank" >Blog</a>
             <a href="https://www.youtube.com/channel/UCXkdaziEd2AXt19WmqvdpQg" target="_blank" >YouTube</a>
             <a href="https://www.threads.net/@king.navneet.singh" target="_blank" >Threads</a>
--- src/components/Instagram.js
+++ src/components/Instagram.js
@@ -7,7 +7,7 @@ import c from '../public/c.jpeg';
 const Instagram = () => (
     <section className="p-8 bg-slate-300 h-1/2 text-gray-800">
         <h2 className="text-3xl font-bold mb-4">Instagram</h2>
-        <a href="https://www.instagram.com/yourinstagramprofile" target="_blank" >Follow me on Instagram for more updates on my journey.</a>
+        <a href="https://www.instagram.com/king.navneet.singh/" target="_blank" >Follow me on Instagram for more updates on my journey.</a>
         <div className='flex flex-row justify-around py-10'>
             <img src={a} alt="Image 1" className="rounded-xl w-1/3 h-66" />
             {/* <img src={b} alt="Image 2" className="rounded-2xl w-1/3 h-66" /> */}
